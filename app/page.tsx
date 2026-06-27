29afc1aa1590a43f6859ae1c84ba1d30f1e4fef8
Akiragon77 <hiroki5959zon@gmail.com>
2026-06-27 20:16:29 +0900

    fix: 執筆エディタ画面に更新

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true, // ログイン状態をブラウザに記憶する
